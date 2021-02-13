d9683d01fd5716821abde20a0ce8b2b57f54a7b7
Sunil <sksunilkumawat1995@gmail.com>
2021-02-14 02:26:30 +0530

    Image added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          SOMETHING AWESOME COMING SOON...
+          SOMETHING AWESOME COMING SOON <img src="https://cdn0.iconfinder.com/data/icons/crime-protection-people-rounded-1/110/Ninja-512.png"></img>
         </p>
       </header>
     </div>
