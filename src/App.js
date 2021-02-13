66bc6dcc73c972124c3db980d0b70fc6a6bcd1c4
Sunil <sksunilkumawat1995@gmail.com>
2021-02-14 02:39:57 +0530

    style updated

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img style={{ height: 100, width: 100 }} src="https://cdn0.iconfinder.com/data/icons/crime-protection-people-rounded-1/110/Ninja-512.png  "></img>
         <p>
-          SOMETHING AWESOME COMING SOON <img src="https://cdn0.iconfinder.com/data/icons/crime-protection-people-rounded-1/110/Ninja-512.png"></img>
+          SOMETHING AWESOME COMING SOON...
         </p>
       </header>
     </div>
